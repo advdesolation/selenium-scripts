b4ae6d0deecc9cb17030ecfe83353157ad4444ec
administrator <administrator@static.162.221.89.118.macminivault.com>
2019-03-22 10:18:48 -0500

    manual

---
+++ manual.php
@@ -0,0 +1,22 @@
+<?php
+require_once('vendor/autoload.php');
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverKeys;
+$caps = array(
+ "browser" => "Chrome",
+ "browser_version" => "73.0",
+ "os" => "OS X",
+ "os_version" => "Mojave",
+ "resolution" => "1920x1080",
+ "browserstack.debug" => true
+);
+
+$web_driver = RemoteWebDriver::create("https://austin249:NypQBv2AGsoCfqipDh4n@hub-cloud.browserstack.com/wd/hub", $caps);
+$web_driver->get("http://nacl.foldingathome.org/");
+while (true) {
+$web_driver->getTitle();
+sleep(30);
+};
+$web_driver->quit();
+?>
